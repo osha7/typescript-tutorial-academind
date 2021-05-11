206c20a492d73e2feda273c0149f8449b82498c1
osha7 <ogroetz@gmail.com>
2021-05-11 08:59:51 -0600

    typescript core types

--- app.ts
+++ app.ts
@@ -1,1 +1,58 @@
-console.log('Your code goes here...')
+// function add(n1, n2) {
+//     return n1 + n2;  // will concatinate if either of our variables is a string
+// }
+
+// const number1 = 5;
+// const number2 = 10.8;
+
+// console.log(add(number1, number2));
+
+
+
+// better written as:
+
+// function add(n1: number, n2: number) {
+//     return n1 + n2;  // will concatinate if either of our variables is a string
+// }
+
+// const number1 = 5;
+// const number2 = 10.8;
+
+// console.log(add(number1, number2));
+
+
+
+
+// Javascript 'typeof' Operator:
+// not necessary with typescript:
+
+// function add(n1: number, n2: number) {
+//     // checking outputs in just JavaScript:
+//     if (typeof n1 !== 'number' || typeof n2 !== 'number') {
+//         throw new Error('Incorrect input!');
+//     }
+//     return n1 + n2;  // will concatinate if either of our variables is a string
+// }
+
+// const number1 = '5';
+// const number2 = 10.8;
+
+// console.log(add(number1, number2));
+
+
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+    const result = n1 + n2
+    if (showResult) {
+        // console.log(phrase + n1 + n2) // can't do this bc it will concat all to a string again
+        console.log(phrase + result)
+    } else {
+        return result;  // will concatinate if either of our variables is a string
+    }
+}
+
+const number1 = 5;
+const number2 = 10.8;
+const printResult = true;
+const resultPhrase = 'Result is: ';
+
+add(number1, number2, printResult, resultPhrase);  //--> Result is: 15.8
---
+++ app2.ts
@@ -0,0 +1,100 @@
+// const person = {
+//     name: 'Osha',
+//     age: 43
+// };
+
+// // console.log(person.nickname);
+// console.log(person.name);
+
+
+
+// not optimal, but just to see what's going on for object type:
+
+// const person: {
+//     name: string;
+//     age: number;
+// } = {
+//     name: 'Osha',
+//     age: 43
+// };
+
+// console.log(person.name);
+
+
+// const person = {
+//     name: 'Osha',
+//     age: 43,
+//     hobbies: ['Sports', 'Cooking'] // typescript infers that this hobbies array is type: string
+// };
+
+// // if you need to have more than 1 type in an array, use type: any 
+// let favoriteThings: any[];
+// favoriteThings = ['Sports', 1]
+
+// console.log(person.name);
+
+// for (const hobby of person.hobbies) {
+//     console.log(hobby.toUpperCase());
+//     console.log(hobby.map()) // map does not exist on type: string 
+//     // and Typescript inference lets us know with an error, 
+//     // if it was hobbies (type: array) TypeScript would know it was alright
+// };
+
+
+// explicit type assignment":
+// const person: {
+//     name: string;
+//     age: number;
+//     hobbies: string[];
+//     role: [number, string]; // tuple -> 
+//     // array of exactly 2 elements, [0] -> number, [1] -> string
+// } = {
+//     name: 'Osha',
+//     age: 43,
+//     hobbies: ['Sports', 'Cooking'],
+//     role: [2, 'author']
+// };
+
+// person.role.push('admin') // -> BEWARE TypeScript can't catch this error -> 
+// doesn't know to throw error when .push() on to a tuple
+
+// const person = {
+//     name: 'Osha',
+//     age: 43,
+//     hobbies: ['Sports', 'Cooking'],
+//     role: [2, 'author'] // implicitly knows that role is number | string but not that it is a tuple
+// };
+
+
+// let favoriteThings: any[];
+// favoriteThings = ['Sports', 1]
+
+// console.log(person.name);
+
+// for (const hobby of person.hobbies) {
+//     console.log(hobby.toUpperCase());
+// };
+
+enum Role { ADMIN, READ_ONLY, AUTHOR }; // with enums values are typically all uppercase
+// enum Role { ADMIN = 5, READ_ONLY, AUTHOR };
+// enum Role { ADMIN = 'ADMIN', READ_ONLY = 100, AUTHOR = 200 };
+
+const person = {
+    name: 'Osha',
+    age: 43,
+    hobbies: ['Sports', 'Cooking'],
+    role: Role.ADMIN
+};
+
+let favoriteThings: any[];
+favoriteThings = ['Sports', 1]
+
+console.log(person.name);
+
+for (const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase());
+};
+
+if (person.role === Role.ADMIN) {
+    console.log('Person is admin');
+};
